e761c27d29fe91329043cb37562bb0e3b0a75fba
rodriguez-miguel-angel <rodriguez.miguel.angel@gmail.com>
2023-03-21 14:02:34 -0400

    added read of crud operations

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,22 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should Read a single Account"""
+        # make a call to self.client.post() to create the account
+        account = self._create_accounts(1)[0]
+        
+        response = self.client.get(
+            f"{BASE_URL}/{account.id}", 
+            json=account.serialize(),
+            content_type="application/json")
+        
+        # assert that the resp.status_code is status.HTTP_200_OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # get the data from resp.get_json()
+        data = response.get_json()
+
+        # Check the data is correct
+        # assert that data["name"] equals the account.name
+        self.assertEqual(data["name"], account.name)
